c7881378ad4714d8e4d48f6d858364b7256afd59
Ionică Bizău <bizauionica@yahoo.com>
2014-07-13 14:26:55 +0300

    Fixed #2

--- utils.js
+++ utils.js
@@ -139,10 +139,61 @@ var Utils = module.exports = {
      *      });
      *
      * */
-  , cloneObject: function (obj) {
-        var c = function () {};
-        c.prototype = Object(obj);
-        return new c();
+  , cloneObject: function cloneObject(item, deepClone) {
+        if (!deepClone) {
+            var c = function () {};
+            c.prototype = Object(item);
+            return new c();
+        }
+
+        if (!item) { return item; } // null, undefined values check
+
+        var types = [ Number, String, Boolean ],
+            result;
+
+        // normalizing primitives if someone did new String('aaa'), or new Number('444');
+        types.forEach(function(type) {
+            if (item instanceof type) {
+                result = type( item );
+            }
+        });
+
+        if (typeof result == "undefined") {
+            if (Object.prototype.toString.call( item ) === "[object Array]") {
+                result = [];
+                item.forEach(function(child, index, array) {
+                    result[index] = cloneObject( child );
+                });
+            } else if (typeof item == "object") {
+                // testing that this is DOM
+                if (item.nodeType && typeof item.cloneNode == "function") {
+                    var result = item.cloneNode( true );
+                } else if (!item.prototype) { // check that this is a literal
+                    if (item instanceof Date) {
+                        result = new Date(item);
+                    } else {
+                        // it is an object literal
+                        result = {};
+                        for (var i in item) {
+                            result[i] = cloneObject( item[i] );
+                        }
+                    }
+                } else {
+                    // depending what you would like here,
+                    // just keep the reference, or create new object
+                    if (false && item.constructor) {
+                        // would not advice to do that, reason? Read below
+                        result = new item.constructor();
+                    } else {
+                        result = item;
+                    }
+                }
+            } else {
+                result = item;
+            }
+        }
+
+        return result;
     }
 }
 
